f2933301498ea2fa3406e74d802f923fb021cad0
Hagar Usama <Hagar_Usama@outlook.com>
2020-07-11 02:26:43 +0200

    *stuck: displaying fetched data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState({ todos: [] });
+ 
+  useEffect(async () => {
+    const result = await axios(
+      //'https://hn.algolia.com/api/v1/search?query=redux',
+     ' https://jsonplaceholder.typicode.com/todos',
+    
+    );
+ 
+    setData(result.data);
+  }, []);
+  console.log(typeof(data))
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+    
+     <ul>
+
+       {data.map(item => (
+         <li key={item.id}>
+           <span> {item.title}</span>
+         </li>
+       ))}
+     </ul>
+
+
     </div>
+    
   );
 }
-
+ 
 export default App;
+
+
+// <ul>
+//       {data.map(item => (
+//         <li key={item.id}>
+//           <span> {item.title}</span>
+//         </li>
+//       ))}
+//     </ul>
